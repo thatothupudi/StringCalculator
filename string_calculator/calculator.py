fd1914ad2bba63f87491405237d760e0fa09ca64
thatothupudi <thato.thupudi@umuzi.org>
2020-05-17 14:26:18 +0200

    Updated

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,93 +1,3 @@
-# def Numbers(var):
-#     return var == '0' or var == '1' or var == '2' or var == '3' or var == '4' or var == '5' or var == '6' or var == '7' or var == '8' or var == '9'
-
-# def Test4Num(varstr):
-#     n = 0
-#     var = ''
-#     try: 
-#         while Numbers(varstr[n]):
-#             var += varstr[n]
-#             n += 1
-#     except: pass
-#     return (int(var), n)
-
-# def operation(string, num1, num2):
-#     if string == '+':
-#         return num1 + num2
-#     if string == '-':
-#         return num1-num2
-#     if string == '*':
-#         return num1*num2
-#     if string == '/':
-#         return num1/num2
-#     if string == '^':
-#         return num1 ** num2
-
-# def operator(operato):
-#     return operato == '+' or operato == '-' or operato == '*' or operato == '/' or operato == '^'
-
-
-# negate = False
-# char = input('')
-
-# print(char + '=')
-# while True:
-#     try: 
-#         if char[0] == '-': #for negative numbers
-#             negate = True #because here the numbers are string format
-#             char = char[1:]
-#         number1 = Test4Num(char)[0]
-#         if negate == True:
-#             number1 = -number1
-#             negate = False
-#         end_number1 = Test4Num(char)[1]
-#         char = char[end_number1:]
-#         if char == '':
-#             print(number1)
-#             break
-#         op = char[0]
-#         char = char[1:]
-#         number2 = Test4Num(char)[0]
-#         end_number2 = Test4Num(char)[1]
-#         result = operation(op, number1, number2)
-#         number1 = result
-#         char = str(number1) + char[end_number2:]
-#         print(char)
-#     except: break
-
-#----------------------------under construction P2
-
-
-# def add(string):
-#   string = _normalize_delimiters(string)
-#   if string:
-#     return _add_numbers_in_string(string)
-#   else:
-#     return 0
-
-# def _normalize_delimiters(string):
-#   string = _normalize_custom_delimiter(string)
-#   string = string.replace('\n', ',')
-#   return string
-
-# def _normalize_custom_delimiter(string):
-#   if string.startswith('//'):
-#     delimiter_spec, string = string.split('\n', 1)
-#     delimiter = delimiter_spec[2:]
-#     string = string.replace(delimiter, ',')
-#   return string
-
-# def _add_numbers_in_string(string):
-#   numbers = map(int, string.split(','))
-#   _validate_numbers(numbers)
-#   return sum(numbers)
-
-# def _validate_numbers(numbers):
-#   if any(number < 0 for number in numbers):
-#     raise ValueError
-
-#----------------------------working
-
 import re
 regex = re.compile(r'\d+') # add one or more digits
 
@@ -104,7 +14,7 @@ def add(string):
     try:
         string[:-1]
     except:
-        raise "This is not ok!"
+        raise "Invalid source of input!"
     if negatives:
         raise Exception("Oops! Negative numbers are not allowed: " + negatives)
     if string == '':
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,41 +1,4 @@
-#--------------------testing under construction------------------
 
-# from string_calculator.calculator import add
-
-# def describe_string_calculator():
-#   def returns_0_for_empty_string():
-#     assert add('') == 0
-
-#   def returns_bare_numbers():
-#     assert add('0') == 0
-#     assert add('1') == 1
-
-#   def adds_numbers():
-#     assert add('1,2') == 3
-#     assert add('1,10') == 11
-
-#   def describe_delimiters():
-#     def can_be_newlines():
-#       assert add('1\n2') == 3
-
-#     def can_be_custom():
-#       assert add('//;\n1;2') == 3
-#       assert add('//+\n1+10') == 11
-#       assert add('//abc\n1abc2abc3')
-
-#     def can_be_mixed():
-#       assert add('//;\n1,2;3\n4') == 10
-
-#     def can_be_minus_signs():
-#       assert add('//-\n1-2') == 3
-
-    # def rejects_negative_numbers():
-    #   assert raise(ValueError, add, '-1')
-    #   assert raise(ValueError, add, '1,-2') == -1
-    
-    
-    #-----------------test that's working
-    
 from string_calculator.calculator import add
 import pytest
 
